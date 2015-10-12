326d02ca8a3429ba28410016b6c32bd5966600b5
mulyoved <mulyoved@gmail.com>
2015-10-12 21:16:51 +0300

    add google services

--- src/android/MyService.java
+++ src/android/MyService.java
@@ -9,18 +9,37 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.app.PendingIntent;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.annotation.TargetApi;
+import android.R;
+
+
 import android.content.Context;
+import android.content.IntentFilter;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.PowerManager;
 import android.util.Log;
 
 import com.red_folder.phonegap.plugin.backgroundservice.BackgroundService;
 
+import de.greenrobot.event.EventBus;
+
 public class MyService extends BackgroundService {
+    public Context context = this;
+	private EventBus eventBus = EventBus.getDefault();
 
 	private final static String TAG = MyService.class.getSimpleName();
+
+    private int notif_id=100;
+    private String notificationTitle = "App Service";
+    private String notificationText = "Running";
 
 	private String mHelloTo = "World";
 	private boolean mReportRealTime = false;
@@ -32,11 +51,19 @@ public class MyService extends BackgroundService {
 	private float mStddev;
 	private float mAvg;
 	private int mStatus = 0;
+	private String activityDetection_status = "unknown";
+	private int activityDetection_confidence = 0;
+	private long activityDetection_time = 0;
 	private float mAvgSampleRate = 0;
 	private long mSampleCount = 0;
 	private float mStepCounter = 0;
 	private int mStep = 0;
     private int mStepWindow = 0;
+
+	private String mActivityStat = "Unknown";
+	private float mActivityConfidence = 0f;
+	private long mActivityLastUpdate = 0;
+
 	List<AccelomterReadingRaw> accelerometerReadingList = new ArrayList<AccelomterReadingRaw>();
 
 
@@ -87,6 +114,7 @@ public class MyService extends BackgroundService {
 		}
 	}
 
+
 	@Override
 	protected JSONObject doWork() {
 		JSONObject result = new JSONObject();
@@ -104,7 +132,9 @@ public class MyService extends BackgroundService {
 			result.put("stepDetector", mStep);
             result.put("stepWindows", mStepWindow);
 
-
+			result.put("activityDetection_status", activityDetection_status);
+			result.put("activityDetection_confidence", activityDetection_confidence);
+			result.put("activityDetection_time", activityDetection_time);
 
 			String statusString = "drive";
 			if (mStatus == 1) statusString = "walk";
@@ -172,10 +202,11 @@ public class MyService extends BackgroundService {
 			if (config.has("report-status-change")) {
                 boolean reportStatusChange = config.getBoolean("report-status-change");
                 if (reportStatusChange && !this.mReportStatusChange) {
-                    this.mReportStatusChange = reportStatusChange;
                     long time = System.currentTimeMillis();
                     this.mReportStatusChangeStartTime = time;
+                    Log.d(TAG, "Reset mReportStatusChangeStartTime");
                 }
+                this.mReportStatusChange = reportStatusChange;
             }
 
 			if (config.has("accelerometerRawBackMs"))
@@ -202,11 +233,126 @@ public class MyService extends BackgroundService {
             if (config.has("stepCountWindow"))
                 this.stepCountWindow = (int) config.getDouble("stepCountWindow");
 
+            if (config.has("notificationTitle")) {
+                this.notificationTitle = config.getString("notificationTitle");
+                Log.d(TAG, "Title: " + this.notificationTitle);
+            }
+            if (config.has("notificationText")) {
+                this.notificationText = config.getString("notificationText");
+                Log.d(TAG, "Text: " + this.notificationText);
+            }
+            updateNotification(this.notificationTitle, this.notificationText);
 
 		} catch (JSONException e) {
 		}
-
 	}
+
+    private Notification getActivityNotification(String title, String text){
+        //Build a Notification required for running service in foreground.
+        Intent main = getApplicationContext().getPackageManager().getLaunchIntentForPackage(getApplicationContext().getPackageName());
+        main.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1000, main,  PendingIntent.FLAG_UPDATE_CURRENT);
+
+        int icon = R.drawable.star_big_on;
+        int normalIcon = getResources().getIdentifier("icon", "drawable", getPackageName());
+        int notificationIcon = getResources().getIdentifier("notificationicon", "drawable", getPackageName());
+        if(notificationIcon != 0) {
+            Log.d(TAG, "Found Custom Notification Icon!");
+            icon = notificationIcon;
+        }
+        else if(normalIcon != 0) {
+            Log.d(TAG, "Found normal Notification Icon!");
+            icon = normalIcon;
+        }
+
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle(title);
+        builder.setContentText(text);
+        builder.setSmallIcon(icon);
+        builder.setContentIntent(pendingIntent);
+        Notification notification;
+        if (android.os.Build.VERSION.SDK_INT >= 16) {
+            notification = buildForegroundNotification(builder);
+        } else {
+            notification = buildForegroundNotificationCompat(builder);
+        }
+        notification.flags |= Notification.FLAG_ONGOING_EVENT | Notification.FLAG_FOREGROUND_SERVICE | Notification.FLAG_NO_CLEAR;
+        return notification;
+    }
+
+    private void updateNotification(String title, String text) {
+        Notification notification = getActivityNotification(title, text);
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(getApplicationContext().NOTIFICATION_SERVICE);
+        mNotificationManager.notify(notif_id, notification);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        notif_id = startId;
+        super.onStartCommand(intent, flags, startId);
+        //handleOnStart(intent);
+        Log.d(TAG, "intent" + intent);
+
+        startForeground(notif_id, getActivityNotification(this.notificationTitle, this.notificationText));
+
+        PowerManager mgr = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wakeLock = mgr.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MyWakeLock");
+        wakeLock.acquire();
+
+        IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mReceiver, filter);
+
+        return START_NOT_STICKY;
+    }
+
+
+	public void onEvent(ActivityMessageEvent event) {
+		Log.d(TAG, "*** ActivityMessageEvent: " + event.status);
+
+		if (mReportRealTime ||
+				(mReportStatusChange && activityDetection_status != event.status)) {
+			activityDetection_status = event.status;
+			activityDetection_confidence = event.confidence;
+			activityDetection_time = event.time;
+			runOnce();
+		}
+	}
+
+
+    void showAlert() {
+        PowerManager mgr = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wakeLock = mgr.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MyWakeLock");
+        wakeLock.acquire();
+
+        //Intent resultIntent = new Intent(context, AlertActivity.class);
+        //resultIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //context.startActivity(resultIntent);
+    }
+
+    public BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                mSensorManager.unregisterListener(sensorEventListener);
+                mSensorManager.registerListener(sensorEventListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), mSensorManager.SENSOR_DELAY_NORMAL);
+                mSensorManager.registerListener(sensorEventListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), mSensorManager.SENSOR_DELAY_NORMAL);
+                mSensorManager.registerListener(sensorEventListener, mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR), mSensorManager.SENSOR_DELAY_NORMAL);
+            }
+        }
+    };
+
+
+    @TargetApi(16)
+    private Notification buildForegroundNotification(Notification.Builder builder) {
+        return builder.build();
+    }
+
+    @SuppressWarnings("deprecation")
+    @TargetApi(15)
+    private Notification buildForegroundNotificationCompat(Notification.Builder builder) {
+        return builder.getNotification();
+    }
 
 	@Override
 	protected JSONObject initialiseLatestResult() {
@@ -216,20 +362,26 @@ public class MyService extends BackgroundService {
 
 	@Override
 	protected void onTimerEnabled() {
+		Log.d(TAG, "*** onTimerEnabled");
+		eventBus.register(this);
+
 		// TODO Auto-generated method stub
 
 		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
 		List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
 
-		//mSensorManager.registerListener(sensorEventListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), mSensorManager.SENSOR_DELAY_NORMAL);
-		//mSensorManager.registerListener(sensorEventListener, mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER), mSensorManager.SENSOR_DELAY_NORMAL);
+		mSensorManager.registerListener(sensorEventListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), mSensorManager.SENSOR_DELAY_NORMAL);
+		mSensorManager.registerListener(sensorEventListener, mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER), mSensorManager.SENSOR_DELAY_NORMAL);
 		mSensorManager.registerListener(sensorEventListener, mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR), mSensorManager.SENSOR_DELAY_NORMAL);
 
 	}
 
 	@Override
 	protected void onTimerDisabled() {
+		Log.d(TAG, "*** onTimerDisabled");
+		eventBus.unregister(this);
+
 		// TODO Auto-generated method stub
 		if (mSensorManager != null) {
 			mSensorManager.unregisterListener(sensorEventListener);
@@ -296,7 +448,7 @@ public class MyService extends BackgroundService {
 		public void onSensorChanged(SensorEvent event) {
 
 			Sensor sensor = event.sensor;
-			if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+			/*if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
 				SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss.SSS");
 				String now = df.format(new Date(System.currentTimeMillis()));
 
@@ -322,39 +474,42 @@ public class MyService extends BackgroundService {
 				collectSensorSample(accReading);
 
 				//we take the value only if is reasonable for walking
-				if (gUnfiltered < highPassFilter) {
-					if (hasPrevValue) {
-						prevx = x * lowPassFilter + prevx * (1 - lowPassFilter);
-						prevy = y * lowPassFilter + prevy * (1 - lowPassFilter);
-						prevz = z * lowPassFilter + prevz * (1 - lowPassFilter);
-					} else {
-						prevx = x;
-						prevy = y;
-						prevz = z;
-						hasPrevValue = true;
-					}
-					final float g = (prevx * prevx + prevy * prevy + prevz * prevz);
-					acceleration = (float) Math.abs(Math.sqrt(g) - gravity);
+				if (hasPrevValue) {
+					prevx = x * lowPassFilter + prevx * (1 - lowPassFilter);
+					prevy = y * lowPassFilter + prevy * (1 - lowPassFilter);
+					prevz = z * lowPassFilter + prevz * (1 - lowPassFilter);
+				} else {
+					prevx = x;
+					prevy = y;
+					prevz = z;
+					hasPrevValue = true;
+				}
+				final float g = (prevx * prevx + prevy * prevy + prevz * prevz);
+				acceleration = (float) Math.abs(Math.sqrt(g) - gravity);
 
 
-                    collectReading(time, acceleration);
+				collectReading(time, acceleration);
 
-					mStddev = calcStdDev(time);
+				mStddev = calcStdDev(time);
 
-					int status = 0;
-					if (mStddev > walkMinStddev && mAvg > walkMinAvg) {
-						status = 1;
-					}
+				int status = 0;
+				if (mStddev > walkMinStddev && mAvg > walkMinAvg) {
+					status = 1;
+				}
 
-					if (mReportRealTime ||
-							(mReportStatusChange && status != mStatus)) {
-						mStatus = status;
-						runOnce();
-					}
+				if (mReportRealTime ||
+						(mReportStatusChange && status != mStatus)) {
+					mStatus = status;
+					runOnce();
 				}
 			} else if (sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
 				mStepCounter = event.values[0];
-			} else if (sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
+				Log.d(TAG, "TYPE_STEP_COUNTER");
+				if (mReportRealTime) {
+					runOnce();
+				}
+			} else */ if (sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
+                Log.d(TAG, "TYPE_STEP_DETECTOR");
                 long time = System.currentTimeMillis();
                 AccelomterReadingRaw accReading = new AccelomterReadingRaw();
                 accReading.x = event.values[0];
@@ -368,15 +523,19 @@ public class MyService extends BackgroundService {
 
                     int status = 0;
                     int stepStart = calcStepCountWindow(stepCountWindow, time);
-                    mStepWindow = mStep - stepStart;
-                    if (mStepWindow > stepCountTrigger) {
-                        status = 1;
-                    }
+                    if (stepStart != 0) {
+                        mStepWindow = mStep - stepStart;
+						/*
+                        if (mStepWindow > stepCountTrigger) {
+                            status = 1;
+                        }
+                        */
 
-                    if (mReportRealTime ||
-                            (mReportStatusChange && status != mStatus)) {
-                        mStatus = status;
-                        runOnce();
+                        if (mReportRealTime ||
+                                (mReportStatusChange && status != mStatus)) {
+                            mStatus = status;
+                            runOnce();
+                        }
                     }
 				}
 			}
@@ -410,6 +569,16 @@ public class MyService extends BackgroundService {
 			Log.d(TAG, "sensorEventListener.onAccuracyChanged");
 		}
 	};
+
+	public void setStatus(String state, float confidence) {
+		mActivityStat = state;
+		mActivityConfidence = confidence;
+		mActivityLastUpdate = System.currentTimeMillis();
+
+		Log.d(TAG, "setStatus " + state);
+		runOnce();
+	}
+
 
 
 }
